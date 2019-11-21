776b8fc0aea47292b9a1668e8f80ac35293f3c6d
Transmissions <greenfeatherhelp@gmail.com>
2019-11-20 17:32:15 -0800

    proficiency_prompt -> chapter_choice

--- src/screens.rs
+++ src/screens.rs
@@ -5,8 +5,22 @@ use cursive::traits::{Boxable, Scrollable};
 use cursive::views::{Dialog, EditView, SelectView};
 use cursive::Cursive;
 
-/// Asks a user to rate their confidence on a specific chapter
-pub fn proficiency_prompt(cursive: &mut Cursive, chapter_num: usize) {
+/// Lets a user choice an action to perform on a given chapter: View study resources or review the chapter
+pub fn chapter_choice(cursive: &mut Cursive, chapter_num: usize) {
+    cursive.add_layer(
+        Dialog::text("What would you like to do?")
+            .button("Review Chapter", move |cursive| {
+                cursive.pop_layer();
+                review_chapter_screen(cursive, chapter_num);
+            })
+            .button("Study Resources", move |cursive| {
+                cursive.pop_layer();
+                study_resources_screen(cursive, chapter_num);
+            }),
+    )
+}
+
+pub fn review_chapter_screen(cursive: &mut Cursive, chapter_num: usize) {
     let mut select = SelectView::new();
     for i in 1..11 {
         select.add_item(i.to_string(), i.to_string());
@@ -43,7 +57,7 @@ pub fn summary_screen(cursive: &mut Cursive) {
     cursive.add_layer(
         Dialog::around(select.on_submit(|cursive, chapter_index| {
             cursive.pop_layer();
-            proficiency_prompt(cursive, *chapter_index);
+            chapter_choice(cursive, *chapter_index);
         }))
         .title("Here's a summary of your learning:")
         .button("Ok", |button_cursive| {
@@ -53,17 +67,30 @@ pub fn summary_screen(cursive: &mut Cursive) {
 }
 
 /// Shows a screen recommending which chapter to review.
-pub fn recommend_review(cursive: &mut Cursive) {
+pub fn recommend_review_screen(cursive: &mut Cursive) {
     let state = cursive.user_data::<State>().unwrap();
 
     if let Some(recommended_chapter_index) = smallest_value_in_hashmap(&state.reviews) {
-        let recommended_chapter = &CHAPTERS[recommended_chapter_index];
-        cursive.add_layer(Dialog::info(
-            recommended_chapter.chapter_recommendation_and_advice(),
-        ));
+        cursive.add_layer(
+            Dialog::info(format!(
+                "You should brush up on Chapter {}",
+                &CHAPTERS[recommended_chapter_index].human_readable
+            ))
+            .button("Study Resources", move |cursive| {
+                cursive.pop_layer();
+                study_resources_screen(cursive, recommended_chapter_index);
+            }),
+        )
     } else {
         cursive.add_layer(Dialog::info("Review some chapters first!"));
     }
+}
+
+pub fn study_resources_screen(cursive: &mut Cursive, chap_num: usize) {
+    let recommended_chapter = &CHAPTERS[chap_num];
+    cursive.add_layer(Dialog::info(
+        recommended_chapter.chapter_resources_string(),
+    ));
 }
 
 /// Shows a prompt where user can type in the name of a chapter. Will redirect user to `profiency_prompt()` if successful. Will not panic if no results are found, shows dialog instead.
@@ -75,7 +102,7 @@ pub fn search_screen(cursive: &mut Cursive) {
                     cursive.pop_layer();
 
                     if let Some(chapter_num) = find_chapter_index(result, CHAPTERS) {
-                        proficiency_prompt(cursive, chapter_num)
+                        chapter_choice(cursive, chapter_num)
                     } else {
                         cursive.add_layer(Dialog::info("No results found."));
                     }
